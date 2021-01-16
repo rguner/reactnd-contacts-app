b094fbc433e39436713a69888e22a9fa8011c57d
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-01-17 01:01:25 +0300

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import PropTypes from 'prop-types'
 
 class App extends Component {
   state = {
@@ -36,11 +37,16 @@ class App extends Component {
       <div>
         <ListContacts
           contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
+          
         />
       </div>
     )
   }
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
 }
 
 export default App
